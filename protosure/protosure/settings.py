b0d6fffaa895a5fd29e37ce83a08a5c0ed0d3802
Faisal <faysalali534@gmail.com>
2022-09-17 22:30:58 +0500

    added new app

--- protosure/protosure/settings.py
+++ protosure/protosure/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'protosure_issue_tracker',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
