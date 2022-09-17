858e63de1625f06a0001d1f9bb5ba9602b554e20
Faisal <faysalali534@gmail.com>
2022-09-17 23:54:05 +0500

    - created middleware - added authentication in middleware.py

---
+++ protosure/protosure/middleware.py
@@ -0,0 +1,21 @@
+import requests
+import json
+from rest_framework.response import Response
+from django.conf import settings
+from django.http import JsonResponse
+from rest_framework import status
+
+
+def github_auth_middleware(get_response):
+    # One-time configuration and initialization.
+
+    def middleware(request):
+        github_token = request.headers.get('authorization')
+        if not github_token:
+            return JsonResponse({'error': 'Token is required to perform this action'}, status=status.HTTP_403_FORBIDDEN)
+
+        response = get_response(request)
+
+        return response
+
+    return middleware
--- protosure/protosure/settings.py
+++ protosure/protosure/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'protosure.middleware.github_auth_middleware'
 ]
 
 ROOT_URLCONF = 'protosure.urls'
